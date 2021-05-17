2039ea898b4a11b15a7063736aea63c6915ba7c0
Lil Devil <themax.06y@gmail.com>
2021-05-17 23:54:52 +0300

    Начало 9 лабораторной

---
+++ lab9/src/main/java/com/example/lab9/GetDate.java
@@ -0,0 +1,42 @@
+package com.example.lab9;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.time.LocalTime;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/task1")
+public class GetDate extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        LocalTime date = LocalTime.now();
+        InetAddress addr = InetAddress.getLocalHost();
+        response.setContentType("text/html");
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        // getData
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"utf-8\">");
+        out.println("</head>");
+        out.println("<body>\n" +
+                "<h1 align=center>" + "Lab 9 task 2a" + "</h1>\n" +
+                "<table border=1 align=center>\n" +
+                "<tr bgcolor=\"#FFAD00\">\n" +
+                "<th>Header Name<th>Header Value" +
+                "<tr><td> Time    <td>" + date.toString() +
+                "<tr><td> Protocol    <td>" + request.getProtocol() +
+                "<tr><td> IP client   <td>" + request.getRemoteAddr() +
+                "<tr><td> IP server   <td>" + addr.getHostAddress() +
+                "<tr><td> IP local name   <td>" + request.getLocalName() +
+                "<tr><td> Port name    <td>" + request.getLocalPort() +
+                "<tr><td> Method name    <td>" + request.getMethod()
+        );
+        out.println("</table></body></html>");
+        out.println("</body></html>");
+    }
+
+}
